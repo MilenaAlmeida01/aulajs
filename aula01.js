dfbdfa31a2ce16bcbbaa6840593806e1242a7c55
Milena Almeida <milenaemanu2014@gmail.com>
2025-07-02 14:46:30 -0300

    Initial commit

---
+++ aula01.js
@@ -0,0 +1,85 @@
+// const teste1 = "" // constante
+// let teste2 = "" // variavel
+
+// function fazerAlgo(parametro1, parametro2) {
+//   // faz algo aqui dentro
+// }
+
+// function soma(numero1, numero2) {
+//   return numero1 + numero2
+// }
+
+// function verificarMaiorQue10(numero) {
+//   if (numero > 10) { // se 
+//     // faz algo
+//     console.log("maior que 10")
+//   } else { // se não
+//     // faz algo
+//     console.log("menor ou igual a 10")
+//   }
+// }
+
+// const resultadoFinal = soma(10, 20)
+// console.log(resultadoFina
+
+// Minha função 
+// function verificarIsMaiorIdade(idade) {
+//   if (idade>18) {
+//     console.log("maior ou igual que 18")
+    
+//   }if (idade<18) {
+//     console.log("menor que 18")
+//   }
+
+//   if (idade == 18) {
+//     console.log('igual a 18')
+//   }
+//}
+
+
+// Alternativa 1
+// function verificarIsMaiorIdade(idade) {
+//   if (idade >= 18) {
+//     console.log("maior ou igual que 18")
+//   } if (idade<18) {
+//     console.log("menor que 18")
+//   } 
+// }
+
+
+// Alternativa 2
+function verificarIsMaiorIdade(idade) {
+  if (idade >= 18) {
+    console.log("maior ou igual que 18")
+  } else {
+    console.log("menor que 18")
+  } 
+}
+
+
+// Alternativa 3
+function verificarIsMaiorIdade(idade) {
+  if (idade>18) {
+    console.log("maior ou igual que 18")
+  } else if (idade<18) {
+    console.log("menor que 18")
+  } else if (idade == 18) {
+    console.log('igual a 18')
+  }
+}
+
+// Alternativa 4
+function verificarIsMaiorIdade(idade) {
+  if (idade>18) {
+    console.log("maior ou igual que 18")
+  } else if (idade<18) {
+    console.log("menor que 18")
+  } else  {
+    console.log('igual a 18')
+  }
+}
+
+let idade = 12
+verificarIsMaiorIdade(47)
+verificarIsMaiorIdade (idade)
+verificarIsMaiorIdade(18)
